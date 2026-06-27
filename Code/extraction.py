73fa8196fad74a8835f9c78187429f3019347dcd
Tadeas Udatny <tadeasudatny250@gmail.com>
2026-06-27 22:58:35 +0200

    Containment class

--- Code/extraction.py
+++ Code/extraction.py
@@ -1,13 +1,28 @@
 from bs4 import BeautifulSoup
 import requests as req
 
-PRESET="https://scp-wiki.wikidot.com/scp"
+PRESET="https://scp-wiki.wikidot.com/scp-"
+
+def pad_number(num: int) -> str:
+    pad_length = 0
+
+    if num < 10:
+        pad_length = 2
+    elif num < 100:
+        pad_length = 1
+
+    parts = ["0" for _ in range(pad_length)]
+    parts.append(str(num))
+    res = "".join(parts)
+
+    print(res)
+    return res
 
 class SCP_item:
-    def __init__(self, number: int, object_class: str):
+    def __init__(self, number: int, containment_class: str):
         self.number = number
-        self.object_class = object_class
-        self.connections: list[int | 'SCP_item'] = []
+        self.containment_class = containment_class
+        self.connections: list[int] = []
 
     def get_connections(self) -> None:
         pass
@@ -28,5 +43,21 @@ def get_SCP_items(lower_bound: int, upper_bound: int) -> dict[int, SCP_item]:
 
     return SCPs
 
-def process_scp(scp: int) -> SCP_item:
-    pass
+def get_containment_class(soup: BeautifulSoup) -> str:
+    containment_element = soup.find_all("strong")[1].parent.text
+    return containment_element.split()[-1]
+
+def process_scp(scp: int) -> SCP_item  | None:
+    link = "".join((PRESET, pad_number(scp)))
+    req_result = req.get("".join(link))
+
+    if req_result.status_code != 200:
+        return None
+    
+    soup = BeautifulSoup(req_result.content, "html.parser")
+
+    containment_class = get_containment_class(soup)
+    
+
+
+    return None
